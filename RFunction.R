7b042a356034c3985f110445f606cc32d0440725
andreakoelzsch <akoelzsch@ab.mpg.de>
2022-09-13 22:20:13 +0200

    test version

--- RFunction.R
+++ RFunction.R
@@ -1,32 +1,22 @@
 library('move')
 library('ctmm')
+library('suncalc')
 
-#data0 <- readRDS("input3_stork.rds")
+#data0 <- readRDS("input2_geese.rds")
 #data <- as.telemetry(data0)
+#saveRDS(data,file="input2_geese_telemetry_list.rds")
+#data <- readRDS("input2_geese_telemetry_list.rds")
 
 # telemetry||telemetry-list to telemetry||telemetry-list
 rFunction <- function(data)
 {
   data <- annotate(data)
   COL <- color(data,by="sun")
+  
+  pdf(paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"), "locations_colored_by_daynight.pdf"))
   plot(data,col=COL)
+  dev.off()
 
   result <- data
   return(result)
 }
-
-# telemetry-list to ctmm-list
-rFunction2 <- function(data)
-{
-  result <- lapply(data,ctmm.fit)
-  return(result)
-}
-  
-  
-  
-  
-  
-  
-  
-  
-  
