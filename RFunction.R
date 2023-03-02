cca2e28a65efcfe936c7d94124bad1d176ad50d7
annescharf <ascharf@ab.mpg.de>
2023-03-02 22:09:25 +0100

    small adition

--- RFunction.R
+++ RFunction.R
@@ -4,6 +4,7 @@ library('ctmm')
 # add legend 
 # add maybe some color() options like color ramp, transparency,
 # add some plot.telemetry options like display error T/F, size of pts,
+# pdf or png, jpg???
 
 rFunction <- function(data, colorby=c("individual", "time", "sun", "moon", "season", "tropic"))
 {
